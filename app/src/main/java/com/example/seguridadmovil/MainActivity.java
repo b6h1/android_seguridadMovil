429da3c34f3b92fd387af66bdd2942e6b139f99c
Luis.Espejo <luis.056628@gmail.com>
2019-11-18 20:50:42 -0500

    seguridad movil - encriptacion
    se agrego la encriptacion y otros aspectos basicos del codigo

--- app/src/main/java/com/example/seguridadmovil/MainActivity.java
+++ app/src/main/java/com/example/seguridadmovil/MainActivity.java
@@ -2,19 +2,70 @@ package com.example.seguridadmovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
 
+
 public class MainActivity extends AppCompatActivity {
+
+
+
+
+    EditText email;
+    EditText contraseña;
+
     private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button iniusu = (Button) findViewById(R.id.button_iniciar);
+        Button registrar = (Button) findViewById(R.id.button_registro);
+        email = findViewById(R.id.edit_email);
+        contraseña = findViewById(R.id.edit_password);
+
+        //--------------------------------------------------------------------
+        iniusu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String usuario_dato = email.getText().toString();
+                String contraseña_dato = contraseña.getText().toString();
+
+                if (usuario_dato.equals("")||contraseña_dato.equals("")){
+                    validacion();
+                }else{
+                    if (usuario_dato.equals("1") && contraseña_dato.equals("1")){
+                        Intent camara = new Intent( MainActivity.this, Home.class);
+                        startActivity(camara);
+
+                    }else{
+                        Toast.makeText(getApplicationContext(),"Usuario Invalido", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent registrar = new Intent( MainActivity.this, Registro.class);
+                startActivity(registrar);
+            }
+        });
+        //----------------------------------------------------------------
+
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
        /*if(currentUser != null){
@@ -39,11 +90,24 @@ public class MainActivity extends AppCompatActivity {
                        }
                    });
        }*/
-}
+    }
 
 
 
 
+
+    private void validacion() {
+
+        String usuario_dato = email.getText().toString();
+        String contraseña_dato = contraseña.getText().toString();
+
+        if (usuario_dato.equals("")){
+            email.setError("Required");
+        }
+        if (contraseña_dato.equals("")){
+            contraseña.setError("Required");
+        }
+    }
 
 
 }
--- app/src/main/java/com/example/seguridadmovil/Registro.java
+++ app/src/main/java/com/example/seguridadmovil/Registro.java
@@ -2,13 +2,138 @@ package com.example.seguridadmovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.InvalidParameterException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Registro extends AppCompatActivity {
+
+    static  String clave = "";
+    EditText registro_email;
+    EditText registro_contraseña;
+    EditText verificar;
+    private static SecretKeySpec secret;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        Button registrar_usuario = (Button) findViewById(R.id.button_registrar);
+        Button volver = (Button) findViewById(R.id.button_volver_login);
+        registro_email = findViewById(R.id.editText_correo);
+        registro_contraseña = findViewById(R.id.editText_contraseña);
+        verificar = findViewById(R.id.editText_verificar_contraseña);
+
+
+        registrar_usuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String usuario_dato = registro_email.getText().toString();
+                String clave = registro_contraseña.getText().toString();
+                String verificar_dato = verificar.getText().toString();
+
+                if (usuario_dato.equals("")||clave.equals("")||verificar_dato.equals("")){
+                    validacion();
+
+                    if(clave == verificar_dato){
+
+                        verificacionContraseña();
+                    }
+                }
+                else{
+                    if (clave.equals("1")){
+                        Intent camara = new Intent( Registro.this, MainActivity.class);
+                        startActivity(camara);
+
+                    }else{
+                        Toast.makeText(getApplicationContext(),"Usuario Invalido", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+
+        volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent volver = new Intent( Registro.this, MainActivity.class);
+                startActivity(volver);
+            }
+        });
+
+
+    }
+
+
+
+    //public static SecretKey generateKey() throws
+
+
+    public static SecretKey generateKey() throws NoSuchAlgorithmException, InvalidKeyException {
+        return secret = new SecretKeySpec(clave.getBytes(), "AES");
+    }
+
+    public static byte[] encryptMsg(String message, SecretKey secret) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidParameterException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
+        Cipher cipher = null;
+        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secret);
+        byte[] cipherText = cipher.doFinal(message.getBytes("UTF-8"));
+        return cipherText;
+    }
+
+
+    public static String decrryptMsg(byte[] cipherText, SecretKey secret) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidParameterException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
+        Cipher cipher = null;
+        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secret);
+        String decryptString = new String(cipher.doFinal(cipherText), "UTF-8");
+        return decryptString;
+    }
+//encryptMsg(contraseña,generateKey());
+
+
+    private void validacion() {
+
+        String usuario_dato = registro_email.getText().toString();
+        String contraseña_dato = registro_contraseña.getText().toString();
+        String verificar_dato = verificar.getText().toString();
+
+        if (usuario_dato.equals("")){
+            registro_email.setError("Required");
+        }
+        if (contraseña_dato.equals("")){
+            registro_contraseña.setError("Required");
+        }
+        if (verificar_dato.equals("")){
+            verificar.setError("Required");
+        }
+    }
+
+    private void verificacionContraseña() {
+        String contraseña_dato = registro_contraseña.getText().toString();
+        String verificacion_dato = verificar.getText().toString();
+
+        if (contraseña_dato != verificacion_dato) {
+            verificar.setError("Required");
+        }
+
     }
 }
